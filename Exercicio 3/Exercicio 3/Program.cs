a7c8a3b730778d64faa90f6c20c41efd2fb2eb9d
sd1vaz <112989098+sd1vaz@users.noreply.github.com>
2023-03-10 20:11:39 -0300

    Add files via upload

---
+++ Exercicio 3/Exercicio 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Exercicio_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> frutas = new List<string>();
+            Console.WriteLine("Digite as frutas separadas por virgula:");
+            string input = Console.ReadLine();
+            string[] frutasarray = input.Split(',');
+            foreach (string fruta in frutasarray)
+            {
+                frutas.Add(fruta.Trim());
+            }
+            Console.WriteLine("\n frutas digitadas:");
+            foreach (string fruta in frutas)
+            {
+                Console.WriteLine(FirstLetterToUpper(fruta));
+            }
+        }
+        static string FirstLetterToUpper(string fruta)
+        {
+            if (string.IsNullOrEmpty(fruta))
+            {
+                return string.Empty;
+            }
+            char[] letters = fruta.ToCharArray();
+            letters[0] = char.ToUpper(letters[0]);
+            return new string(letters);
+        }
+    }
+}
