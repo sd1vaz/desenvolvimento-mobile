45a1fa44ee2f7dba124099c060598c37584d7fe2
sd1vaz <112989098+sd1vaz@users.noreply.github.com>
2023-03-10 00:58:06 -0300

    Add files via upload

---
+++ Exercicio2-lista/Exercicio2-lista/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio2_lista
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> convidados = new List<string>();
+            while (true) 
+            {
+
+                Console.WriteLine("insira o nome do convidado");
+                string nome = Console.ReadLine();
+                if (string.IsNullOrEmpty(nome))
+                {
+                    break;
+                }
+                convidados.Add(nome);   
+            }           
+            if (convidados.Count > 0)
+            {
+                for (int i = 0; i < convidados.Count; i++)
+                {
+                    Console.WriteLine( convidados[i]);
+                }
+            }
+        }
+            
+        
+    }
+}
