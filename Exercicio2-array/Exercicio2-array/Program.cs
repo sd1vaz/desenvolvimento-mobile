c09877cae02c0995a5277f5b0403a830aa3c4991
sd1vaz <112989098+sd1vaz@users.noreply.github.com>
2023-03-10 00:30:59 -0300

    Add files via upload

---
+++ Exercicio2-array/Exercicio2-array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio2_array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] convidados = new string[20];
+            for (int posiçao = 0; posiçao < convidados.Length; posiçao++)
+            {
+                Console.Write(" informe o nome do convidado: ");
+                convidados[posiçao] = (Console.ReadLine());
+                if (string.IsNullOrEmpty(convidados[posiçao])) 
+                {
+                    break;
+                }
+            }
+           
+            Console.WriteLine("Convidados informados:");
+            for (int posiçao = 0;posiçao < convidados.Length;posiçao++)
+            {
+                Console.WriteLine(convidados[posiçao]);
+            }
+               
+                
+               
+              
+            Console.ReadKey();      
+        }
+    }
+}
